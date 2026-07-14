ab8c76cf84f674959d7ae1b496a3ead56e30d715
Helder Barboza <29435727+helderbarboza@users.noreply.github.com>
2026-07-13 22:01:11 -0400

    Perform validations on public fun

--- lib/weather_br/weather/open_meteo.ex
+++ lib/weather_br/weather/open_meteo.ex
@@ -15,7 +15,28 @@ defmodule WeatherBr.Weather.OpenMeteo do
     |> Req.new()
   end
 
-  def fetch_forecast(city_name, lat, lon, days \\ 6) do
+  def fetch_forecasts(cities, days \\ 6)
+
+  def fetch_forecasts(_cities, days) when days not in 1..7 do
+    raise ArgumentError,
+          "days must be an integer between 1 and 7 (inclusive), got: #{inspect(days)}"
+  end
+
+  def fetch_forecasts([] = cities, _days) do
+    raise ArgumentError, "cities list must not be empty, got: #{inspect(cities)}"
+  end
+
+  def fetch_forecasts(cities, days) when is_list(cities) and is_integer(days) do
+    cities
+    |> Task.async_stream(
+      fn {city_name, lat, lon} -> fetch_forecast(city_name, lat, lon, days) end,
+      max_concurrency: 5,
+      timeout: 10_000
+    )
+    |> Enum.map(fn {:ok, result} -> result end)
+  end
+
+  defp fetch_forecast(city_name, lat, lon, days) do
     req =
       new(
         params: [
@@ -29,26 +50,5 @@ defmodule WeatherBr.Weather.OpenMeteo do
     response = Req.get!(req)
     %{"daily" => %{"temperature_2m_max" => temperatures}} = response.body
     {city_name, Enum.take(temperatures, days)}
-  end
-
-  def fetch_forecasts(cities, days \\ 6)
-
-  def fetch_forecasts([] = cities, _days) do
-    raise ArgumentError, "cities list must not be empty, got: #{inspect(cities)}"
-  end
-
-  def fetch_forecasts(_cities, days) when days not in 1..7 do
-    raise ArgumentError,
-          "days must be an integer between 1 and 7 (inclusive), got: #{inspect(days)}"
-  end
-
-  def fetch_forecasts(cities, days) when is_list(cities) and is_integer(days) do
-    cities
-    |> Task.async_stream(
-      fn {city_name, lat, lon} -> fetch_forecast(city_name, lat, lon, days) end,
-      max_concurrency: 5,
-      timeout: 10_000
-    )
-    |> Enum.map(fn {:ok, result} -> result end)
   end
 end
